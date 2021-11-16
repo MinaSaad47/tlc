a579e08d43ea71206886bcd4c7b9d0703769550c
MinaSaad47 <MinaSaad-47@outlook.com>
2021-11-16 18:49:08 +0200

    prepared main

--- Main.cs
+++ Main.cs
@@ -14,7 +14,21 @@ public class MainClass
         
         string src = File.ReadAllText(args[0]);
 
-        Console.WriteLine("Tiny SRC:\n");
+        Console.Write(String.Concat(Enumerable.Repeat("=", 33)));
+        Console.Write("Tiny SRC");
+        Console.WriteLine(String.Concat(Enumerable.Repeat("=", 33)));
+
         Console.WriteLine(src);
+
+        TLC.Scanner scanner = new TLC.Scanner();
+        scanner.StartScanning(src);
+
+        foreach(TLC.Token token in scanner.Tokens)
+        {
+            Console.WriteLine($"{token.lex}\t{token.token_type}");
+        }
+        
+
+        Console.WriteLine(String.Concat(Enumerable.Repeat("=", 80)));
     }
 }
