137f050b0dc68e5b18b7a2d7488b6fac8689def3
MinaSaad47 <MinaSaad-47@outlook.com>
2021-12-30 16:05:57 +0200

    cfg

--- Parser.cs
+++ Parser.cs
@@ -27,40 +27,96 @@ namespace TLC
 		{
 			this.InputPointer = 0;
 			this.TokenStream = TokenStream;
-			root = new Node("Program");
+			root = new Node("Tree");
 			root.Children.Add(Program());
 			return root;
 		}
+
 		Node Program()
 		{
 			Node program = new Node("Program");
-			program.Children.Add(Header());
-			program.Children.Add(DeclSec());
-			program.Children.Add(Block());
+			program.Children.Add(FuncStmt());
+			// program.Children.Add(MainFunc());
 			// program.Children.Add(match(TK.Dot));
 			// MessageBox.Show("Success");
 			return program;
 		}
-		
-		Node Header()
+
+		Node FuncStmt()
 		{
-			Node header = new Node("Header");
-			// write your code here to check the header sructure
-			return header;
+			Node node = new Node("FuncStmt");
+			node.Children.Add(FuncDecl());
+			node.Children.Add(FuncBody());
+
+			return node;
 		}
-		Node DeclSec()
+
+		Node FuncDecl()
 		{
-			Node declsec = new Node("DeclSec");
-			// write your code here to check atleast the declare sturcure 
-			// without adding procedures
-			return declsec;
+			Node node = new Node("FuncDecl");
+			node.Children.Add(DataType());
+			node.Children.Add(FuncName());
+			node.Children.Add(match(TK.L_Paren));
+			// node.Children.Add(ParamList());
+			node.Children.Add(match(TK.R_Paren));
+			return node;
 		}
-		Node Block()
+
+		Node FuncName()
 		{
-			Node block = new Node("block");
-			// write your code here to match statements
-			return block;
+			Node node = new Node("FuncName");
+			node.Children.Add(match(TK.Identifier));
+			return node;
 		}
+
+		Node FuncBody()
+		{
+			Node node = new Node("FuncBody");
+			return node;
+		}
+
+		Node DataType()
+		{
+			Node node = new Node("DataType");
+			switch (TokenStream[InputPointer].token_type)
+			{
+			case TK.Int:
+				node.Children.Add(match(TK.Int));
+				break;
+			case TK.Float:
+				node.Children.Add(match(TK.Float));
+				break;
+			case TK.String:
+				node.Children.Add(match(TK.String));
+				break;
+			default:
+				return null;
+			}
+			return node;
+		}
+
+
+
+		// 
+		// Node Header()
+		// {
+		// 	Node header = new Node("Header");
+		// 	// write your code here to check the header sructure
+		// 	return header;
+		// }
+		// Node DeclSec()
+		// {
+		// 	Node declsec = new Node("DeclSec");
+		// 	// write your code here to check atleast the declare sturcure 
+		// 	// without adding procedures
+		// 	return declsec;
+		// }
+		// Node Block()
+		// {
+		// 	Node block = new Node("block");
+		// 	// write your code here to match statements
+		// 	return block;
+		// }
 
 		// Implement your logic here
 
---
+++ tasks/cfg.md
@@ -0,0 +1,45 @@
+```
+Program					-> FuncStmt Main_Func | Main_Func
+FuncStmt				-> FuncDecl FuncBody
+FuncDecl				-> DataType FuncName (ParamList)
+FuncName				-> identifier
+ParamList				-> Param ParamList' | ParamList'
+ParamList'			-> , Param ParamList' | e
+Param						-> DataType identifier
+DataType				-> int | float | string
+FuncBody				-> { Statements RetStmt }
+Statements			-> Statement Statements'
+Statements'			-> Statement Statements' | e
+Statement				-> FuncCall | AssignStmt | DeclStmt | WriteStmt | ReadStmt | RetStmt | IfElseStmt | RepeatStmt | CommentStmt |  e
+CommentStmt			-> comment
+FuncCall				-> FuncName (ArgList) ;
+ArgList					-> Arg ArgList' | ArgList'
+ArgList'				-> , Arg ArgList' | e
+Arg							-> indentifier
+AssignStmt			-> LValue := RValue ;
+LValue					-> identifier
+RValue					-> string | Expression
+Expression			-> Term Expression'
+Expression'			-> AddOp Term Expression' | e
+AddOp						-> + | -
+Term						-> Factor Term'
+Term'						-> MulOp Factor Term' | e
+MulOp						-> * | /
+Factor					-> number | identifier | FuncCall
+DeclStmt				-> DataType IdentList ;
+IdentList				-> identifier IdentList'
+IdentList'			-> , identifier IdentList' | e
+WriteStmt				-> write RValue ;
+ReadStmt				-> read LValue ;
+RetStmt					-> return RValue ;
+RepeatStmt			-> repeat Statements until CondStmt
+CondStmt				-> Condition CondStmt'
+CondStmt'				-> BoolList Condition CondStmt' | e
+Condition				-> Expression CondOp Expression
+CondOp					-> < | > | = | <>
+BoolList				-> && | "||"
+IfElseStmt			-> IfBlock CondStmt then Statements ElseBlock end
+ElseBlock				-> ElIfBlock ElseBlock | else Statments | e
+ElIfBlock				-> elseif CondStmt then Statements
+MainFunc				-> Datatype main () FuncBody
+```
